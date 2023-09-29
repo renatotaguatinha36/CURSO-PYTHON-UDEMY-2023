da3d43ed06d80bfefe5d40b02791a8c096e56424
renatoguara2020 <renatoguara2020@gmail.com>
2023-09-29 09:38:16 -0300

    ok

---
+++ python/python/listas.py
@@ -0,0 +1,18 @@
+lista = ['RENATO','SANDRA','CREUZA','FERNANDO','DUDU','VANDA','GUIMA','RUBIANA','ANDRÉ','ANA JULIA','CAMILLE','RENATO','RENATO','RENATO']
+
+# print(lista[::2])
+# print(dir(lista))
+# lista.reverse()
+# print(lista)
+
+lista2 = lista.copy()
+
+# print(lista2)
+
+# lista2.append("Caroline Barbosa Banza de Arruda")
+# lista2.append("Nathan Soares Barbosa")
+# lista2.remove('SANDRA')
+# lista2.pop(3)
+print(len(lista2))
+print(lista2.count('RENATO'))
+print(lista2.index('RENATO'))
